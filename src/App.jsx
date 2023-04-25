52d03ee15c8457857bd374dd911983e01dbee55a
tristan <tristan.wermelinger@gmail.com>
2023-04-25 15:14:23 +0200

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
 
 function App() {
-  const App = () => {
-    return <h2>Reprendre avec</h2>;
-  };
+  return (
+    <>
+      <h2>Emissions TV</h2>
+      <h2>Séries françaises</h2>
+      <h2>Comédies</h2>
+      <h2>Nouveautés</h2>
+      <h2>Documentaires</h2>
+    </>
+  );
 }
 
 export default App;
